fa3fc390cbaf713c34c7cc120425243d5d25f899
sebastian calderon <bl.calderon@udla.edu.co>
2020-07-22 17:59:28 -0500

    home

--- src/scenes/Layout/Public/Public.js
+++ src/scenes/Layout/Public/Public.js
@@ -3,12 +3,14 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import  {Auth}  from '../../Auth/AuthDealer';
 import { SingUp } from "../../Client/SingUp/SingUp";
 import { auth } from '../../../services/Auth/AuthActions';
+import { Home } from '../../../scenes/Client/Home/Home';
 
 export const Public = () => {
   return (
     <Router>
       <Switch className="h-100">
-        <Route exact path="/" component={Auth} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/home" component={Home} />
         <Route exact path="/login" component={Auth} />
         <Route exact path="/login-admin" component={Auth} />
         <Route path="/sing-up" component={SingUp} />
